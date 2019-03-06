1abd3b0f8e6177fa6f17d7d75254c97d183e3135
bernevek <bernevek1995@ukr.net>
2019-03-06 23:52:12 +0200

    deploy to azure (fix db init 2)

--- server/src/main/resources/data.sql
+++ server/src/main/resources/data.sql
@@ -5,8 +5,8 @@ VALUES(1, "WORD", "winword.exe"),
 (4, "password", "password"),
 (5, "finger print", "fingerPrint"),
 (6, "vk","vk.com"),
-(7, "fb","facebook.com"),
-(8, "mail.ru","mail.ru");
+(7, "fb","www.facebook.com"),
+(8, "google","www.google.com");
 
 INSERT IGNORE INTO application(id)
 VALUES(1),
@@ -28,16 +28,3 @@ VALUES(1, "Default user policy");
 INSERT IGNORE INTO user_policy(id)
 VALUES(1);
 
-INSERT IGNORE INTO policy_apps(policy_id, app_id)
-VALUES(1, 1),
-(1, 2),
-(1, 3);
-
-INSERT IGNORE INTO policy_login_methods(policy_id, login_method_id)
-VALUES(1, 4),
-(1, 5);
-
-INSERT IGNORE INTO policy_sites(policy_id, site_id)
-VALUES(1, 6),
-(1, 7),
-(1, 8);
