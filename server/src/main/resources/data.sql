714bbd8381e85f8e78f14440e72fc7643797e545
bernevek <bernevek1995@ukr.net>
2019-03-06 23:27:08 +0200

    deploy to azure (fix db init)

--- server/src/main/resources/data.sql
+++ server/src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO config_element(id, label, value)
+INSERT IGNORE INTO config_element(id, label, value)
 VALUES(1, "WORD", "winword.exe"),
 (2, "NOTEPAD", "notepad.exe"),
 (3, "EXEL", "exel.exe"),
@@ -8,36 +8,36 @@ VALUES(1, "WORD", "winword.exe"),
 (7, "fb","facebook.com"),
 (8, "mail.ru","mail.ru");
 
-INSERT INTO application(id)
+INSERT IGNORE INTO application(id)
 VALUES(1),
 (2),
 (3);
 
-INSERT INTO login_method(id)
+INSERT IGNORE INTO login_method(id)
 VALUES(4),
 (5);
 
-INSERT INTO site(id)
+INSERT IGNORE INTO site(id)
 VALUES(6),
 (7),
 (8);
 
-INSERT INTO base_policy(id, name)
+INSERT IGNORE INTO base_policy(id, name)
 VALUES(1, "Default user policy");
 
-INSERT INTO user_policy(id)
+INSERT IGNORE INTO user_policy(id)
 VALUES(1);
 
-INSERT INTO policy_apps(policy_id, app_id)
+INSERT IGNORE INTO policy_apps(policy_id, app_id)
 VALUES(1, 1),
 (1, 2),
 (1, 3);
 
-INSERT INTO policy_login_methods(policy_id, login_method_id)
+INSERT IGNORE INTO policy_login_methods(policy_id, login_method_id)
 VALUES(1, 4),
 (1, 5);
 
-INSERT INTO policy_sites(policy_id, site_id)
+INSERT IGNORE INTO policy_sites(policy_id, site_id)
 VALUES(1, 6),
 (1, 7),
 (1, 8);
